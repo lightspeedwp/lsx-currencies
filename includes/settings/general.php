b4f84dbd729aa4123104c8867db6881e584cc6e6
Fernando Tessmann <nandotess85@gmail.com>
2017-10-25 11:20:30 -0500

    Added compatibility with LSX Search

--- includes/settings/general.php
+++ includes/settings/general.php
@@ -3,13 +3,29 @@
 		<?php if ( class_exists( 'LSX_Currencies' ) ) { ?>
 			<li><a href="#ui-currencies" class="active"><?php esc_html_e( 'Currencies', 'lsx-currencies' ); ?></a></li>
 		<?php } ?>
+
+		<?php if ( class_exists( 'LSX_Search' ) ) { ?>
+			<?php $class_active = class_exists( 'LSX_Currencies' ) ? '' : 'active' ?>
+			<li><a href="#ui-search" class="<?php echo esc_attr( $class_active ) ?>"><?php esc_html_e( 'Search', 'lsx-currencies' ); ?></a></li>
+		<?php } ?>
 	</ul>
 
 	<?php if ( class_exists( 'LSX_Currencies' ) ) { ?>
 		<div id="ui-currencies" class="ui-tab active">
-			<table class="form-table" style="margin-top:-13px !important;">
+			<table class="form-table">
 				<tbody>
 					<?php do_action( 'lsx_framework_dashboard_tab_content', 'currency_switcher' ); ?>
+				</tbody>
+			</table>
+		</div>
+	<?php } ?>
+
+	<?php if ( class_exists( 'LSX_Search' ) ) { ?>
+		<?php $class_active = class_exists( 'LSX_Currencies' ) ? '' : 'active' ?>
+		<div id="ui-search" class="ui-tab <?php echo esc_attr( $class_active ) ?>">
+			<table class="form-table">
+				<tbody>
+					<?php do_action( 'lsx_framework_dashboard_tab_content', 'search' ); ?>
 				</tbody>
 			</table>
 		</div>
