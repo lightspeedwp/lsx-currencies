7061120fed216a93b26eda89d0f8c35ce9163132
Virginia Garcia <ve.garcia.ortiz@gmail.com>
2020-10-14 15:15:27 -0300

    - Removed the deprecated tabs on the theme options.

--- includes/settings/display.php
+++ includes/settings/display.php
@@ -1,48 +1,17 @@
 <div class="uix-field-wrapper">
 	<ul class="ui-tab-nav">
-		<?php if ( class_exists( 'LSX_Banners' ) ) { ?>
-			<li><a href="#ui-placeholders" class="active"><?php esc_html_e( 'Placeholders', 'lsx-currencies' ); ?></a></li>
-		<?php } ?>
 
 		<?php if ( class_exists( 'LSX_Currencies' ) ) { ?>
 			<?php $class_active = class_exists( 'LSX_Banners' ) ? '' : 'active'; ?>
 			<li><a href="#ui-currencies" class="<?php echo esc_attr( $class_active ); ?>"><?php esc_html_e( 'Currencies', 'lsx-currencies' ); ?></a></li>
 		<?php } ?>
 
-		<?php if ( class_exists( 'LSX_Team' ) ) { ?>
-			<?php $class_active = ( class_exists( 'LSX_Banners' ) || class_exists( 'LSX_Currencies' ) ) ? '' : 'active'; ?>
-			<li><a href="#ui-team" class="<?php echo esc_attr( $class_active ); ?>"><?php esc_html_e( 'Team', 'lsx-currencies' ); ?></a></li>
-		<?php } ?>
-
-		<?php if ( class_exists( 'LSX_Testimonials' ) ) { ?>
-			<?php $class_active = ( class_exists( 'LSX_Banners' ) || class_exists( 'LSX_Currencies' ) || class_exists( 'LSX_Team' ) ) ? '' : 'active'; ?>
-			<li><a href="#ui-testimonials" class="<?php echo esc_attr( $class_active ); ?>"><?php esc_html_e( 'Testimonials', 'lsx-currencies' ); ?></a></li>
-		<?php } ?>
-
-		<?php if ( class_exists( 'LSX_Projects' ) ) { ?>
-			<?php $class_active = ( class_exists( 'LSX_Banners' ) || class_exists( 'LSX_Currencies' ) || class_exists( 'LSX_Team' ) || class_exists( 'LSX_Testimonials' ) ) ? '' : 'active'; ?>
-			<li><a href="#ui-projects" class="<?php echo esc_attr( $class_active ); ?>"><?php esc_html_e( 'Projects', 'lsx-currencies' ); ?></a></li>
-		<?php } ?>
 
 		<?php if ( class_exists( 'LSX_Services' ) ) { ?>
 			<?php $class_active = ( class_exists( 'LSX_Banners' ) || class_exists( 'LSX_Currencies' ) || class_exists( 'LSX_Team' ) || class_exists( 'LSX_Testimonials' ) || class_exists( 'LSX_Projects' ) ) ? '' : 'active'; ?>
 			<li><a href="#ui-services" class="<?php echo esc_attr( $class ); ?>"><?php esc_html_e( 'Services', 'lsx-currencies' ); ?></a></li>
 		<?php $class = ''; } ?>
 
-		<?php if ( class_exists( 'LSX_Blog_Customizer' ) ) { ?>
-			<?php $class_active = ( class_exists( 'LSX_Banners' ) || class_exists( 'LSX_Currencies' ) || class_exists( 'LSX_Team' ) || class_exists( 'LSX_Testimonials' ) || class_exists( 'LSX_Projects' ) || class_exists( 'LSX_Services' ) ) ? '' : 'active'; ?>
-			<li><a href="#ui-blog-customizer" class="<?php echo esc_attr( $class_active ); ?>"><?php esc_html_e( 'Blog Customizer (posts widget)', 'lsx-currencies' ); ?></a></li>
-		<?php } ?>
-
-		<?php if ( class_exists( 'LSX_Sharing' ) ) { ?>
-			<?php $class_active = ( class_exists( 'LSX_Banners' ) || class_exists( 'LSX_Currencies' ) || class_exists( 'LSX_Team' ) || class_exists( 'LSX_Testimonials' ) || class_exists( 'LSX_Projects' ) || class_exists( 'LSX_Services' ) || class_exists( 'LSX_Blog_Customizer' ) ) ? '' : 'active'; ?>
-			<li><a href="#ui-sharing" class="<?php echo esc_attr( $class_active ); ?>"><?php esc_html_e( 'Sharing', 'lsx-currencies' ); ?></a></li>
-		<?php } ?>
-
-		<?php if ( class_exists( 'LSX_Videos' ) ) { ?>
-			<?php $class_active = ( class_exists( 'LSX_Banners' ) || class_exists( 'LSX_Currencies' ) || class_exists( 'LSX_Team' ) || class_exists( 'LSX_Testimonials' ) || class_exists( 'LSX_Projects' ) || class_exists( 'LSX_Services' ) || class_exists( 'LSX_Blog_Customizer' ) || class_exists( 'LSX_Sharing' ) ) ? '' : 'active'; ?>
-			<li><a href="#ui-videos" class="<?php echo esc_attr( $class_active ); ?>"><?php esc_html_e( 'Videos', 'lsx-currencies' ); ?></a></li>
-		<?php } ?>
 	</ul>
 
 	<?php if ( class_exists( 'LSX_Banners' ) ) { ?>
