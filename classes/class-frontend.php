df63b042ebf5499c62dd6e13aabb3d3b160e3794
Warwick <krugazul@gmail.com>
2021-10-20 11:38:21 +0200

    Updating the money format function

--- classes/class-frontend.php
+++ classes/class-frontend.php
@@ -181,9 +181,9 @@ class Frontend {
 				$value = preg_replace( '/' . preg_quote( '.', '/' ) . '/', '', $value, $decimals );
 			}
 
-			$money_format = '%i';
+			$money_format = 2;
 			if ( false !== lsx_currencies()->remove_decimals ) {
-				$money_format = '%.0n';
+				$money_format = 0;
 			}
 
 			$prefix .= '>';
@@ -191,9 +191,15 @@ class Frontend {
 
 			setlocale( LC_MONETARY, 'en_US' );
 
-			// Work out the other tags
+			// Work out the other tags.
 			$currency = '<span class="currency-icon ' . mb_strtolower( lsx_currencies()->base_currency ) . '">' . lsx_currencies()->base_currency . '</span>';
-			$amount = '<span class="value" data-price-' . lsx_currencies()->base_currency . '="' . trim( str_replace( array( '$', 'USD' ), '', money_format( (float) $money_format, ltrim( rtrim( $value ) ) ) ) ) . '" ' . $additional_html . '>' . str_replace( array( '$', 'USD' ), '', money_format( $money_format, ltrim( rtrim( $value ) ) ) ) . '</span>';
+
+			$value = ltrim( rtrim( $value ) );
+			
+			$for_value = number_format( (float) $value, $money_format );
+			$for_value = str_replace( array( '$', 'USD' ), '', $for_value );
+			
+			$amount = '<span class="value" data-price-' . lsx_currencies()->base_currency . '="' . trim( str_replace( array( '$', 'USD' ), '', $value ) ) . '" ' . $additional_html . '>' . str_replace( array( '$', 'USD' ), '', $for_value ) . '</span>';
 
 			// Check for a price type and add that in.
 			$price_type = get_post_meta( get_the_ID(), 'price_type', true );
