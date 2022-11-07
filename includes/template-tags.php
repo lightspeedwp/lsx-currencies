557e13ba4ecacfb1963d9eb962548c1793ba79c0
Warwick <krugazul@gmail.com>
2022-11-07 14:22:26 +0200

    Updating the amoutn value template tag.

--- includes/template-tags.php
+++ includes/template-tags.php
@@ -15,10 +15,13 @@
  * @param string $value
  * @return void
  */
-function lsx_currencies_get_price_html( $value = '' ) {
-	$prefix = '<span class="amount lsx-currencies" ';
-	$value = preg_replace( '/[^0-9.]+/', '', $value );
+function lsx_currencies_get_price_html( $value = '', $post_id = false ) {
+	$prefix   = '<span class="amount lsx-currencies" ';
+	$value    = preg_replace( '/[^0-9.]+/', '', $value );
 	$decimals = substr_count( $value, '.' );
+	if ( false !== $post_id ) {
+		$post_id = get_the_ID();
+	}
 
 	$money_format = 2;
 	if ( false !== lsx_currencies()->remove_decimals ) {
@@ -34,13 +37,20 @@ function lsx_currencies_get_price_html( $value = '' ) {
 	$suffix = '</span>';
 	setlocale( LC_MONETARY, 'en_US' );
 
+	// Set the prices to use the base currency set on the tour.
+	$currency      = lsx_currencies()->base_currency;
+	$tour_currency = get_post_meta( $post_id, 'currency', true );
+	if ( false !== $tour_currency && '' !== $tour_currency ) {
+		$currency = strtoupper( $tour_currency );
+	}
+
 	// Work out the other tags
-	$currency = '<span class="currency-icon ' . mb_strtolower( lsx_currencies()->base_currency ) . '">' . lsx_currencies()->base_currency . '</span>';
+	$currency = '<span class="currency-icon ' . mb_strtolower( $currency ) . '">' . $currency . '</span>';
 
 	$formatted_amount = number_format( (float) $value, $money_format );
 	$formatted_amount = str_replace( array( '$', 'USD' ), '', $formatted_amount );
 
-	$amount = '<span class="value" data-price-' . lsx_currencies()->base_currency . '="' . trim( str_replace( 'USD', '', $formatted_amount ) ) . '">' . str_replace( 'USD', '', $formatted_amount ) . '</span>';
+	$amount = '<span class="value" data-price-' . $currency . '="' . trim( str_replace( 'USD', '', $formatted_amount ) ) . '">' . str_replace( 'USD', '', $formatted_amount ) . '</span>';
 	$price_html = '<span class="amount lsx-currencies">' . $currency . $amount . '</span>';
 	return $price_html;
 }
