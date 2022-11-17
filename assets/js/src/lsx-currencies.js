9a4401829fc82e926a3d680391992ec13517fef2
Warwick <krugazul@gmail.com>
2022-11-18 01:58:25 +0200

    Updating the currency js

--- assets/js/src/lsx-currencies.js
+++ assets/js/src/lsx-currencies.js
@@ -70,10 +70,11 @@ LSX_Currencies = {
                 jQuery(this).html(currency_span);
             } else {
                 jQuery(this).find('.value').html(new_price);
-                jQuery(this).find('.currency-icon').prop('class', '').addClass('currency-icon').addClass($this.current_currency.toLowerCase()).html($this.current_currency);
-
+                jQuery(this).find('.currency-icon').prop('class', '').addClass('currency-icon').addClass($this.current_currency.toLowerCase());
+				if ( true === lsx_currencies_params.currency_tag ) {
+					jQuery(this).find('.currency-icon').html($this.current_currency);
+				}
 			}
-
         });
 	},
 
